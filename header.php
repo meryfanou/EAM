1fb627bbdf845be9dee86e883ebf9bb411008c6c
meryfanou <mery.fanou@gmail.com>
2020-12-18 14:40:31 +0200

    navbar index.php

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+</head>
+<body>
+
+    <!-- Custom styles for this template -->
+    <link href="navbar-top-fixed.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="./index.php"><img src="Logo.png"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item active">
+        	<form class="d-flex">
+		        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        		<button class="btn btn-outline-success" type="submit">Search</button>
+		    </form>
+        </li>
+        <li class="nav-item">
+        	<a class="nav-link" href="#">Σύνδεση</a>
+        </li>
+        <li class="nav-item">
+        	<a class="nav-link" href="#">Εγγραφή</a>
+        </li>
+        <li class="nav-item">
+        	<a class="nav-link" href="#">English</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
