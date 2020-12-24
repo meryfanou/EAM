f23e00ca0aa8f829891d82024baba99ece93aa40
meryfanou <mery.fanou@gmail.com>
2020-12-24 17:42:18 +0200

    add header and footer

--- footer.php
+++ footer.php
@@ -1,5 +1,74 @@
 
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
 
+<footer class="text-center text-lg-start" style="background-color: rgba(0, 0, 0, 0.3)">
+<!-- Grid container -->
+<div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+     <!--Grid column-->
+    	<div class="col">
+        	<h5 class="text-uppercase">ωρες λειτουργιας</h5>
+	        <hr style="border-top: 2px solid rgb(14, 160, 195); width: 80%;">
+    	    <ul class="list-unstyled mb-0">
+        		<li>
+          			<p>ΔΕΥΤΕΡΑ-ΠΑΡΑΣΚΕΥΗ......08:00-14:00</p>
+        		</li>
+	        	<li>
+    	      		<p>ΣΑΒΒΑΤΟ.............................09:00-13:00</p>
+        		</li>
+         		<li>
+	          		<p>ΚΥΡΙΑΚΕΣ / ΓΙΟΡΤΕΣ / ΑΡΓΙΕΣ: ΚΛΕΙΣΤΑ</p>
+    	     	</li>
+        	</ul>
+	    </div>
+
+    	<!--Grid column-->
+	    <div class="col">
+    	    <h5 class="text-uppercase">τοποθεσια</h5>
+        	<hr style="border-top: 2px solid rgb(14, 160, 195); width: 80%;">
+	        <ul class="list-unstyled mb-0">
+    		    <li>
+	          		<p><i class="fas fa-map-marker-alt"></i>&nbsp;Σταδίου 29, Αθήνα 105 59</p>
+    		    </li>
+	          	<li>
+	            	<a href="#!" class="text-dark"><i class="fas fa-map"></i>&nbsp;Οδηγίες Πρόσβασης</a>
+    	    	</li>
+        	</ul>
+      	</div>
+
+    	<!--Grid column-->
+	    <div class="col">
+    	    <h5 class="text-uppercase mb-0">επικοινωνια</h5>
+        	<hr style="border-top: 2px solid rgb(14, 160, 195); width: 80%;">
+        	<ul class="list-unstyled">
+          		<li>
+          			<p><i class="fas fa-phone"></i>&nbsp;213-1516649</p>
+          		</li>
+          		<li>
+        	  		<p><i class="fas fa-phone"></i>&nbsp;213-1516651</p>
+    	      	</li>
+	          	<li>
+            		<a href="" class="text-dark"><i class="fas fa-envelope"></i>&nbsp;pliroforisi-politi@ypakp.gr</a>
+	        	</li>
+        	</ul>
+    	</div>
+    </div>
+</div>
+
+<!-- Copyright -->
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+    © 2020 Copyright:
+    <a class="text-dark" href="https://www.ypakp.gr/">ΥΠΟΥΡΓΕΙΟ ΕΡΓΑΣΙΑΣ</a>
+</div>
+</footer>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,41 +1,98 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title></title>
+	<title>ΥΠΟΥΡΓΕΙΟ ΕΡΓΑΣΙΑΣ</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+ 	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
+
 <body>
 
-    <!-- Custom styles for this template -->
-    <link href="navbar-top-fixed.css" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<!-- Navbar -->
+<nav class="navbar navbar-expand-sm bg-info navbar-light fixed-top" style="height: 110px;">
+<!-- Logo -->
+<div class="col">
+	<a class="navbar-brand" href="./index.php">
+    	<img src="Logo.png" alt="Logo" style="height: 55px;">
+  	</a>
+</div>
 
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="./index.php"><img src="Logo.png"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <li class="nav-item active">
-        	<form class="d-flex">
-		        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        		<button class="btn btn-outline-success" type="submit">Search</button>
-		    </form>
-        </li>
-        <li class="nav-item">
-        	<a class="nav-link" href="#">Σύνδεση</a>
-        </li>
-        <li class="nav-item">
-        	<a class="nav-link" href="#">Εγγραφή</a>
-        </li>
-        <li class="nav-item">
-        	<a class="nav-link" href="#">English</a>
-        </li>
-      </ul>
-    </div>
-  </div>
+<!-- Dropdowns -->
+<div class="col">
+	<ul class="navbar-nav" style="position: relative; margin-left: -3%; margin-top: 8%;">
+		<li class="nav-item dropdown">
+	    	<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">ΕΡΓΑΖΟΜΕΝΟΙ</a>
+      		<div class="dropdown-menu">
+		        <a class="dropdown-item" href="#">COVID-19 ΚΑΙ ΕΡΓΑΖΟΜΕΝΟΙ</a>
+       			<a class="dropdown-item" href="#">ΑΜΟΙΒΕΣ / ΔΩΡΑ / ΑΔΕΙΕΣ</a>
+	    	    <!-- <a class="dropdown-item" href="#">Link 3</a> -->
+   			</div>
+	    </li>
+   		<li class="nav-item dropdown">
+	    	<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">&nbsp;&nbsp;ΕΡΓΟΔΟΤΕΣ</a>
+      		<div class="dropdown-menu">
+		        <a class="dropdown-item" href="#">COVID-19 ΚΑΙ ΕΡΓΟΔΟΤΕΣ</a>
+       			<a class="dropdown-item" href="#">ΥΠΟΛΟΓΙΣΜΟΣ ΑΣΦΑΛΙΣΤΙΚΩΝ ΕΙΣΦΟΡΩΝ ΕΠΙΧΕΙΡΗΣΗΣ</a>
+	    	    <!-- <a class="dropdown-item" href="#">Link 3</a> -->
+   			</div>
+	    </li>
+		<li class="nav-item dropdown">
+	    	<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">&nbsp;&nbsp;ΑΝΕΡΓΟΙ</a>
+   			<div class="dropdown-menu">
+	        	<a class="dropdown-item" href="#">ΘΕΣΕΙΣ ΕΡΓΑΣΙΑΣ</a>
+        		<a class="dropdown-item" href="#">ΕΠΙΔΟΜΑΤΑ</a>
+		        <a class="dropdown-item" href="#">ΕΥΚΑΙΡΙΕΣ ΚΑΤΑΡΤΗΣΗΣ</a>
+   			</div>
+    	</li>
+    	<li class="nav-item dropdown">
+		    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">&nbsp;&nbsp;ΣΥΝΤΑΞΙΟΥΧΟΙ</a>
+   			<div class="dropdown-menu">
+		        <a class="dropdown-item" href="#">ΝΕΑ ΓΙΑ ΤΙΣ ΣΥΝΤΑΞΕΙΣ</a>
+       			<!-- <a class="dropdown-item" href="#">Link 2</a> -->
+	    	    <!-- <a class="dropdown-item" href="#">Link 3</a> -->
+   			</div>
+    	</li>
+	</ul>
+</div>
+
+<!-- Other Options -->
+<div class="col">
+	<ul class="navbar-nav mb-3" style="position: relative; margin-left: -5%;">
+		<li class="nav-item">
+	    	<a class="nav-link" href="#" style="font-size: 87%;">ΣΥΝΔΕΣΗ</a>
+		</li>
+		<li class="nav-item">
+		    <a class="nav-link" href="#" style="font-size: 87%;">|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ΕΓΓΡΑΦΗ</a>
+    	</li>
+   		<li class="nav-item">
+   			<a class="nav-link" href="#" style="font-size: 87%;">|&nbsp;&nbsp;&nbsp;ΕΠΙΚΟΙΝΩΝΙΑ</a>
+	    </li>
+		<li class="nav-item">
+   			<a class="nav-link" href="#" style="font-size: 87%;">|&nbsp;&nbsp;&nbsp;English&nbsp;<i class="fa fa-language"></i></a>
+	    </li>
+	</ul>
+
+	<!-- Search form -->
+	<div class="row"><div class="col-sm-4"></div><div class="col">
+  		<form class="form-inline" action="" style="width: 140%;">
+  			<div class="input-group input-group-sm mb-2">
+				<input type="text" class="form-control" placeholder="Search">
+				<div class="input-group-append input-group-sm">
+		    		<button class="btn btn-secondary" type="submit"><i class="fa fa-search"></i></button>
+		  		</div>
+			</div>
+		</form>
+	</div></div>
+</div>
 </nav>
-
-
